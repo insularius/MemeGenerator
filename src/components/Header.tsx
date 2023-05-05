7990cb9fdf7e87b69728ed2ccf4dd5d38dadf785
Sultanbek <sultan.sekenov@yandex.ru>
2023-05-05 16:06:44 +0600

    deleted trash

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,22 +7,3 @@ export default function Navbar() {
     </header>
   );
 }
-
-// "prettier.singleQuote": true,
-//   "editor.formatOnSave": true,
-//   "eslint.format.enable": true,
-//   "files.autoSave": "onFocusChange",
-//   "editor.defaultFormatter": "esbenp.prettier-vscode",
-//   "editor.suggestSelection": "first"
-
-// echo "# MemeGenerator" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin git@github.com:insularius/MemeGenerator.git
-// git push -u origin main
-
-// git remote add origin git@github.com:insularius/MemeGenerator.git
-// git branch -M main
-// git push -u origin main
