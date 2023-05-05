4588f27a9eabbaa8e680f4252f1118d7a589121c
Sultanbek <sultan.sekenov@yandex.ru>
2023-05-05 12:21:49 +0600

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.png" className="header-image" alt="" />
+      <h2 className="header-title">Meme Generator</h2>
+      {/* <h4 className="header-text">React Course - Project 3</h4> */}
+    </header>
+  );
+}
+
+// "prettier.singleQuote": true,
+//   "editor.formatOnSave": true,
+//   "eslint.format.enable": true,
+//   "files.autoSave": "onFocusChange",
+//   "editor.defaultFormatter": "esbenp.prettier-vscode",
+//   "editor.suggestSelection": "first"
+
+// echo "# MemeGenerator" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:insularius/MemeGenerator.git
+// git push -u origin main
+
+// git remote add origin git@github.com:insularius/MemeGenerator.git
+// git branch -M main
+// git push -u origin main
